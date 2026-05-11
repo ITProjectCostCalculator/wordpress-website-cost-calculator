c637a6c60193025686bdea3ff48db2b0fb9bb039
ITProjectCostCalculator <seo.shivlab@gmail.com>
2026-05-11 18:32:32 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>Get Instant Quote and Cost of IT Project Development with AI</title>
+  <body>
+    <h1>List of top Calculatros users are using</h1>
+    <p>this is just test webiste, which is currently under development, Please give us some time we will publish here something very cool.</p>
+  </body>
+</head>
+  
+</html>
